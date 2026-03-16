98914328ecac95cca761ceae940c2d50b5ec9424
Julian <84198069+JulianKranz135@users.noreply.github.com>
2026-03-16 10:12:48 +0100

    The Page

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+        let targets = {
+            'pause': new Date("Mar 16, 2026 11:05:00").getTime(),
+            'school': new Date("Mar 16, 2026 12:50:00").getTime(), 
+            'vacation': new Date("Mar 27, 2026 12:50:00").getTime() 
+        };
+
+        setInterval(function() {
+            const now = new Date().getTime();
+
+            calculateTime('pause', now);
+            calculateTime('school', now);
+            calculateTime('vacation', now);
+
+        }, 1000);
+
+        function calculateTime(id, now) {
+            const distance = targets[id] - now;
+            const container = document.getElementById('cd-' + id);
+
+            if (distance < 0) {
+                container.innerHTML = "<h2>JETZT!</h2>";
+                return;
+            }
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            container.querySelector('.day').innerText = days;
+            container.querySelector('.hour').innerText = formatTime(hours);
+            container.querySelector('.min').innerText = formatTime(minutes);
+            container.querySelector('.sec').innerText = formatTime(seconds);
+        }
+
+        function formatTime(time) {
+            return time < 10 ? `0${time}` : time;
+        }
+
+        function updateTarget(id) {
+            const input = document.getElementById('input-' + id).value;
+            
+            if (input) {
+                targets[id] = new Date(input).getTime();
+                
+                const container = document.getElementById('cd-' + id);
+                if (container.querySelector('h2')) {
+                    location.reload(); 
+                }
+            } else {
+                alert("Bitte wähle zuerst ein Datum und eine Uhrzeit aus!");
+            }
+        }
